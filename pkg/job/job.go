d79b24b7d0ae76cc6f137a5e9527c4e90b2bf15b
pleimer <pfbleimer@gmail.com>
2020-05-08 13:58:36 -0400

    Adjust job file

--- pkg/job/job.go
+++ pkg/job/job.go
@@ -2,8 +2,10 @@ package job
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
+	"github.com/infrawatch/ci-server-go/pkg/ghclient"
 	"github.com/infrawatch/ci-server-go/pkg/logging"
 )
 
@@ -11,4 +13,21 @@ import (
 type Job interface {
 	Run(context.Context, *sync.WaitGroup)
 	SetLogger(*logging.Logger)
+}
+
+// Factory generate jobs based on event type
+func Factory(event ghclient.Event, client *ghclient.Client) (Job, error) {
+	switch e := event.(type) {
+	case *ghclient.Push:
+		l, err := logging.NewLogger(logging.ERROR, "console")
+		if err != nil {
+			return nil, err
+		}
+		return &PushJob{
+			event:  e,
+			client: client,
+			Log:    l,
+		}, nil
+	}
+	return nil, fmt.Errorf("could not determine github event type")
 }
