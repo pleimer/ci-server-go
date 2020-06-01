f81d99b5b45bfd7bf353bee953fb9f2199421d01
pleimer <pfbleimer@gmail.com>
2020-05-31 20:00:02 -0400

    k8s resources

--- run-on-ocp.sh
+++ run-on-ocp.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+if [[ -z "${GITHUB_USER}" ]]; then 
+    echo "GITHUB_USER must be specified"
+    exit -1
+fi
+
+
+if [[ -z "${GITHUB_OAUTH}" ]]; then 
+    echo "GITHUB_OAUTH must be specified"
+    exit -1
+fi
+
 sed "s/<oauth_token>/$(cat ~/accessgit)/g" deploy-on-k8s.yml > /tmp/ci-ocp.yml
+sed "s/<github_user>/${GITHUB_USER}/g" /tmp/ci-ocp.yml > /tmp/ci-ocp.yml
 oc create -f /tmp/ci-ocp.yml
